71ee95174bc30967d11eea0e867966f9dc425e68
MokiEss <mokhtar.essaid@uha.fr>
2022-04-07 12:31:28 +0200

    CEC 2017

--- DEV2.py
+++ DEV2.py
@@ -6,19 +6,20 @@ from cec2017.functions import all_functions
 
 # functions are defined for 10, 30, 50 and 100 dimensions
 #the main optimize of DE
-def optimize():
+def optimize(num_function,D):
     #initialize parameters and necessary data
-    num_function = 0
-    D = 100
+    #num_function = 29
+    #D = 50
     UB, LB = bd.Define_boundaries_CEC2017(D)
-    NP = D*14#Population size
+    NP = D*16#Population size
     maxNP = NP #max population size 
-    minNP = 6  #min population size
-    mu_archive_size = 3
+    minNP = 4 #min population size
+    mu_archive_size = 5
     mu_by_dimension =  np.zeros((mu_archive_size,D)) + (1/D)
+    mu_by_dimensionCR = np.zeros((mu_archive_size,D)) + (1/D)
     index_mu_by_dimension = 0
-    F_P = np.zeros((NP,D)) + 0.5    #mutation parameter
-    CR_P = np.zeros(NP) + 0.5 #crossover parameter
+    F_P = np.zeros((NP,D)) + 0.8    #mutation parameter
+    CR_P = np.zeros((NP,D)) + 0.8#crossover parameter
     Evaluation_number = 20000*D
     Population = np.zeros((NP,D)) #original population  
     mutated_population = np.zeros((NP,D)) # mutated population
@@ -67,11 +68,11 @@ def optimize():
      
       # 2- cross
       #CR_P = np.random.uniform(size=NP)
-      #eigen_population, eigen_mutated, eigen = DE.getEigenmatrix(Population,mutated_population)
-      crossed_population = DE.crossover(Population,mutated_population, crossed_population, NP, D, CR_P)
-      #eigen_crossed_population = DE.crossover(eigen_population,eigen_mutated, eigen_crossed_population, NP, D, CR_P)
+      eigen_population, eigen_mutated, eigen = DE.getEigenmatrix(Population,mutated_population,D)
+      #crossed_population = DE.crossover(Population,mutated_population, crossed_population, NP, D, CR_P)
+      eigen_crossed_population = DE.crossover(eigen_population,eigen_mutated, eigen_crossed_population, NP, D, CR_P)
       # return the eigen crossed population to the original coordinates
-      #crossed_population = np.matmul(eigen_crossed_population, eigen.T )
+      crossed_population = np.matmul(eigen_crossed_population, eigen.T )
       # 3- boundary handling
       crossed_population = DE.boundaries_handling_CEC2017(UB, LB, crossed_population,D, num_function, NP)
     
@@ -83,10 +84,11 @@ def optimize():
      
       # 5- Select the fitest solutions for the next generation, determine the successfull F parameters and compute the fitness differences
       #Population, fitness_population, difference_fitness = DE.Selection(crossed_population, Population, fitness_population, fitness_crossed, NP, F_P,D)
-      Population, fitness_population, difference_fitness,Nb_successful_parameters,SF_P,Successful_parents = DE.Selection(crossed_population, Population, fitness_population, fitness_crossed, NP, F_P,D)
+      Population, fitness_population, difference_fitness,Nb_successful_parameters,SF_P,SCR_P,Successful_parents = DE.Selection(crossed_population, Population, fitness_population, fitness_crossed, NP, F_P,CR_P,D)
       number_of_improved_solution = Nb_successful_parameters
       # 6- Generate new F values for each dimension and solution based on Cauchy distribution
-      sF_P,mu_by_dimension,index_mu_by_dimension = DE.Generate_F_parameter(difference_fitness,D,F_P,NP,Nb_successful_parameters,SF_P,mu_by_dimension,index_mu_by_dimension,mu_archive_size,Successful_parents, Population)
+
+      F_P,mu_by_dimension,mu_by_dimensionCR,index_mu_by_dimension = DE.Generate_F_CR_parameter(difference_fitness,D,F_P,CR_P,NP,Nb_successful_parameters,SF_P,SCR_P,mu_by_dimension,mu_by_dimensionCR,index_mu_by_dimension,mu_archive_size,Successful_parents, Population)
 
       # 7- Get rid of a fraction of the worst solution 
       Population, fitness_population, NP = DE.Reduce_population(Population, fitness_population, NP, maxNP, minNP, it, Evaluation_number)
@@ -95,10 +97,10 @@ def optimize():
       if (fitness_best_solution>np.amin(fitness_population)):
           fitness_best_solution = np.amin(fitness_population)
           index_min = np.where(fitness_population == np.amin(fitness_population))
-          BestKnown_solution = Population[int(index_min[0]),:]   
+          BestKnown_solution = Population[index_min[0][0],:]
       it = it + NP
-      print("the best solution so far is", fitness_best_solution, "current population size is", NP) 
-      
+      #print("the best solution so far is", fitness_best_solution, "current population size is", NP)
+    return fitness_best_solution
        
-optimize()
+
 
--- test.py
+++ test.py
@@ -1,5 +1,80 @@
-# Using only f5:
-from cec2017.functions import all_functions
+import csv
+import numpy as np
+import DEV2
+# open the file in the write mode
 
-print(len(all_functions))
 
+result10D = np.zeros((30,25))
+result30D = np.zeros((30,25))
+result50D = np.zeros((30,25))
+result100D = np.zeros((30,25))
+
+print("experimentation on 10D")
+D = 10
+for i in range(30):
+    print("function ",i, "dimension", D)
+    for j in range(25):
+        result10D[i,j] = DEV2.optimize(i,D)
+
+print("experimentation on 30D")
+D = 30
+for i in range(30):
+    print("function ", i, "dimension", D)
+    for j in range(25):
+        result30D[i,j] = DEV2.optimize(i,D)
+print("experimentation on 50D")
+D = 50
+for i in range(30):
+    print("function ", i, "dimension", D)
+    for j in range(25):
+        result50D[i,j] = DEV2.optimize(i,D)
+print("experimentation on 100D")
+D = 100
+for i in range(30):
+    print("function ", i, "dimension", D)
+    for j in range(25):
+        result100D[i,j] = DEV2.optimize(i,D)
+
+f = open('results10DCEC2017.csv', 'w')
+
+# create the csv writer
+writer = csv.writer(f)
+
+# write a row to the csv file
+writer.writerow(result10D)
+
+# close the file
+f.close()
+
+f = open('results30DCEC2017.csv', 'w')
+
+# create the csv writer
+writer = csv.writer(f)
+
+# write a row to the csv file
+writer.writerow(result30D)
+
+# close the file
+f.close()
+
+f = open('results50DCEC2017.csv', 'w')
+
+# create the csv writer
+writer = csv.writer(f)
+
+# write a row to the csv file
+writer.writerow(result50D)
+
+# close the file
+f.close()
+
+f = open('results100DCEC2017.csv', 'w')
+
+# create the csv writer
+writer = csv.writer(f)
+
+# write a row to the csv file
+writer.writerow(result100D)
+
+# close the file
+f.close()
